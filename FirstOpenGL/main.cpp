642c2cc1aacaaa6094f7a264cb0140eebbf0d511
Unknown <cedricschwyter@bluewin.ch>
2018-05-31 20:36:03 +0200

    In the process of comiling the shaders

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -9,6 +9,20 @@
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\0";
+
 //render a simple triangle
 float vertices[] = {
 	-0.5f, -0.5f, 0.0f,
@@ -59,6 +73,58 @@ int main()
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+	}
+
+	unsigned int VBO;
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+
+	int successVertexShader;
+	char infoLogVertexShader[512];
+
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &successVertexShader);
+	if (!successVertexShader) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLogVertexShader);
+		std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLogVertexShader << std::endl;
+	}
+
+	unsigned int fragmentShader;
+
+	int successFragmentShader;
+	char infoLogFragmentShader[512];
+
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &successFragmentShader);
+	if (!successFragmentShader) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLogFragmentShader);
+		std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLogFragmentShader << std::endl;
+	}
+	
+	unsigned int shaderProgram;
+
+	int successShaderProgram;
+	char infoLogShaderProgram[512];
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &successShaderProgram);
+	if (!successShaderProgram) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLogShaderProgram);
+		std::cerr << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLogShaderProgram << std::endl;
 	}
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
